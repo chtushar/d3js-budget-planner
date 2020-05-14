a7ea7b46801b27d66740c07b6b1a8e5176ccd097
chtushar <choudharit23@gmail.com>
2020-05-15 00:24:54 +0530

    completed

--- graph.js
+++ graph.js
@@ -13,28 +13,80 @@ const pie = d3.pie()
             .sort(null)
             .value(d => d.cost)
 
-const angles = pie([
-    {name: 'rent', cost: 500},
-    {name: 'bills', cost: 300},
-    {name: 'gaming', cost: 200}
-])
 
 const arcPath = d3.arc()
                 .outerRadius(dims.radius)
                 .innerRadius(dims.radius/2);
 
+const color = d3.scaleOrdinal(d3['schemeSet3'])
+
+
+const legendGroup = svg.append('g')
+                       .attr('transform', `translate(${dims.width + 40}, 10)`)
+
+const legend = d3.legendColor()
+                .shape('circle')
+                .shapePadding(10)
+                .scale(color);
+
+const tip = d3.tip()
+            .attr('class', 'tip card')
+            .html(d => {
+                let content = `<div class="name">${d.data.name}</div>
+                               <div class="cost">${d.data.cost}</div>
+                               <div class="delete">Click to Delete</div>`
+                return content
+            });
+
+graph.call(tip)
 
 const update = (data) => {
     
+
+    color.domain(data.map(d => d.name));
+
+    legendGroup.call(legend)
+    legendGroup.selectAll('text')
+                .attr('fill','white')
+
     const paths = graph.selectAll('path')
                   .data(pie(data));
+
+
+    //handle the exit selection
+    paths.exit()
+    .transition().duration(750)
+    .attrTween('d', arcTweenExit)
+        .remove()
+
+    
+
+
+    paths.attr('d', arcPath)
+         .transition().duration(750)
+         .attrTween('d', arcTweenUpdate)
 
     paths.enter()
         .append('path')
         .attr('class', 'arc')
-        .attr('d', arcPath)
         .attr('stroke', '#fff')
         .attr('stroke-width', 3)
+        .attr('fill', d => color(d.data.name))
+        .each(function(d){this._current = d})
+        .transition().duration(750)
+        .attrTween("d", arcTweenEnter)
+
+    //add events
+    graph.selectAll('path')
+         .on('mouseover', (d, i, n) =>{
+            tip.show(d, n[i]);
+            handleMouseOver(d,i,n);
+         })
+         .on('mouseout', (d, i, n) => {
+            tip.hide();
+            handleMouseOut(d, i, n);
+         })
+         .on('click', handleClick)
 
 }
 
@@ -61,8 +113,54 @@ db.collection('expenses').onSnapshot(res => {
                     break;
                 default:
                     break;
-            }
-        });
-
-        update(data)
+                }
+            });
+            
+            update(data)
 })
+const arcTweenEnter = (d) => {
+    let i = d3.interpolate(d.endAngle, d.startAngle);
+
+    return function(t){
+        d.startAngle = i(t);
+        return arcPath(d)
+    }
+};
+
+const arcTweenExit = (d) => {
+    let i = d3.interpolate(d.startAngle, d.endAngle);
+
+    return function(t){
+        d.startAngle = i(t);
+        return arcPath(d)
+    }
+};
+
+function arcTweenUpdate(d) {
+    
+    let i = d3.interpolate(this._current, d)
+
+    this._current = i(1)
+
+    return( function(t) {
+        return arcPath(i(t))
+    })
+
+}
+
+const handleMouseOver = (d, i, n) => {
+    d3.select(n[i])
+    .transition('changeSliceFill').duration(300)
+    .attr('fill', '#fff')
+}
+
+const handleMouseOut = (d, i, n) => {
+    d3.select(n[i])
+    .transition('changeSliceFill').duration(300)
+    .attr('fill', color(d.data.name))
+}
+
+const handleClick = (d) => {
+    const id = d.data.id;
+    db.collection('expenses').doc(id).delete()
+}
